2d5d9ebfe706de4672799eaf8feae0f2e74c769f
tienvv1234 <tienvv1234@gmail.com>
2022-01-08 17:28:12 +0700

    update

--- src/api/trackerApi.js
+++ src/api/trackerApi.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: 'http://061a-2402-800-6105-6184-8067-c1ad-1db1-e3b9.ngrok.io',
+    baseURL: 'http://9d4e-27-72-97-105.ngrok.io',
     headers: {
         Accept: 'application/json',
         'Content-Type': 'application/json',
